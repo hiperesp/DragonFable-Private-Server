8b042fd05a8b654e504cdc3d896265ca6e898838
Gabriel Lopes <gabriel@gabstep.com.br>
2024-08-11 04:22:09 +0000

    Initial Commit (mockup)

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" id="FFable" codebase="https://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="750" height="550" align="middle">
+    <param name="allowScriptAccess" value="sameDomain" />
+    <param name="movie" value="DFLoader.swf" />
+    <param name="menu" value="false" />
+    <param name="allowFullScreen" value="true" />
+    <param name="flashvars" value="" />
+    <param name="bgcolor" value="#530000" />
+    <embed src="DFLoader.swf" FLASHVARS="" name="FFable" bgcolor="#530000" menu="false"  
+        allowFullScreen="true" width="750" height="550" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" 
+        pluginspage="https://www.macromedia.com/go/getflashplayer" swLiveConnect="true" />
+</object>
