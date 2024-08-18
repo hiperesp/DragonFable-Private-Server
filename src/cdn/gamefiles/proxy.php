2fff651e9836f1b9ee13148d66767245e3f6ade4
hiperesp <gabriel@gabstep.com.br>
2024-08-18 20:49:35 -0300

    simulate windows serving files

--- src/cdn/gamefiles/proxy.php
+++ src/cdn/gamefiles/proxy.php
@@ -1,25 +1,30 @@
 <?php
 
 $requestPath = \preg_replace('/\/cdn\/gamefiles\//', '', $_SERVER['REQUEST_URI']);
+$localFile = __DIR__."/".\strtolower(\urldecode($requestPath));
 
-$ch = \curl_init();
-\curl_setopt($ch, CURLOPT_URL, "https://play.dragonfable.com/game/gamefiles/{$requestPath}");
-\curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-\curl_setopt($ch, CURLOPT_HEADER, 0);
-$response = \curl_exec($ch);
-$httpcode = \curl_getinfo($ch, CURLINFO_HTTP_CODE);
-$contentType = \curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-\curl_close($ch);
+if(!\file_exists($localFile)) {
+    $ch = \curl_init();
+    \curl_setopt($ch, CURLOPT_URL, "https://play.dragonfable.com/game/gamefiles/{$requestPath}");
+    \curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    \curl_setopt($ch, CURLOPT_HEADER, 0);
+    $response = \curl_exec($ch);
+    $httpcode = \curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $contentType = \curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+    \curl_close($ch);
 
-if ($httpcode == 200) {
-    $path = __DIR__ . '/' . \strtolower(\urldecode($requestPath));
-    $dir = \dirname($path);
+    if ($httpcode != 200) {
+        \http_response_code(404);
+        echo 'File not found';
+        exit;
+    }
+
+    $dir = \dirname($localFile);
     if (!\is_dir($dir)) {
         \mkdir($dir, 0777, true);
     }
-    \file_put_contents($path, $response);
+    \file_put_contents($localFile, $response);
 }
-
-header('Content-Type: ' . $contentType);
-\http_response_code($httpcode);
-echo $response;
+$contentType = \mime_content_type($localFile);
+\header("Content-Type: {$contentType}");
+\readfile($localFile);
