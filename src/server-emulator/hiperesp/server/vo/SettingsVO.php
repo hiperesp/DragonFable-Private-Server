087ec38759b4bcb6caabe061236a36000349d3f4
Gabriel <gabriel@gabstep.com.br>
2024-08-20 07:29:17 -0300

    removed df.hiper.esp.br cdn

--- src/server-emulator/hiperesp/server/vo/SettingsVO.php
+++ src/server-emulator/hiperesp/server/vo/SettingsVO.php
@@ -23,8 +23,11 @@ class SettingsVO extends ValueObject {
 
     public function __construct() {
         $this->gameVersion    = "game15_8_05-patched.swf";
-        $this->serverLocation = "server-emulator/server.php/";          // http://localhost:40000/server-emulator/server.php/
-        $this->gamefilesPath  = "https://df.hiper.esp.br/gamefiles/";   // http://localhost:40000/cdn/gamefiles/
+        $this->serverLocation = "server-emulator/server.php/";
+        // $this->serverLocation = "http://localhost:40000/server-emulator/server.php/";
+        $this->gamefilesPath  = "cdn/gamefiles/";
+        // $this->gamefilesPath  = "http://localhost:40000/cdn/gamefiles/";
+        // $this->gamefilesPath  = "https://df.hiper.esp.br/gamefiles/";
 
         $this->homeUrl          = "../../index.hmtl";
         $this->playUrl          = "../../play.html";
