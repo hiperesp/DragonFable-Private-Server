d17dbed004b99331ddcb6c8f11e68d0502adb25f
hiperesp <gabriel@gabstep.com.br>
2024-08-27 22:50:27 -0300

    New feature: Auto patch new swf versions

---
+++ dev-tools/patch-new-swf/patch-swf.php
@@ -0,0 +1,86 @@
+<?php
+
+$replaces = [
+    "\"http://www.dragonfable.com/df-activation.asp\"" => "_root.conn.url + \"web/df-activation.asp\"",
+    "\"https://www.dragonfable.com/df-chardetail.asp?id=\"" => "_root.conn.url + \"web/df-chardetail.asp?id=\"",
+    "\"https://www.dragonfable.com/amulet/df-upgrade3.asp?CharID=\"" => "_root.conn.url + \"web/df-upgrade3.asp?CharID=\"",
+    "\"http://www.dragonfable.com/amulet/df-upgrade3.asp?CharID=\"" => "_root.conn.url + \"web/df-upgrade3.asp?CharID=\"",
+    "\"<a href=\'https://www.dragonfable.com/df-lostpassword.aspx\'><u>Forgot your password</u></a>?\"" => "\"<a href=\'\" + _root.conn.url + \"web/df-lostpassword.aspx\'><u>Forgot your password</u></a>?\"",
+    "\"<a href=\'https://www.dragonfable.com/df-signup.asp\'><u>Create a new account.</u></a>\"" => "\"<a href=\'\" + _root.conn.url + \"web/df-signup.asp\'><u>Create a new account.</u></a>\"",
+    "\"http://dragonfable.battleon.com/game/cf-dacheck.asp\"" => "_root.conn.url + \"cf-dacheck.asp\"",
+    "\"https://dragonfable.battleon.com/game/cf-dacheck.asp\"" => "_root.conn.url + \"cf-dacheck.asp\"",
+    <<<'ACTIONSCRIPT'
+    _root.conn = new Object();
+    if(_url.indexOf("file://") == -1)
+    {
+       _root.conn.url = "";
+    }
+    else
+    {
+       _root.conn.url = "https://dragonfable.battleon.com/game/";
+    }
+    if(_root.onfacebook != undefined)
+    {
+       _root.conn.url = "https://dragonfable.battleon.com/game/";
+    }
+    _root.conn.login = function(strUsername, strPassword)
+    ACTIONSCRIPT => <<<'ACTIONSCRIPT'
+    _root.conn = new Object();
+    _root.conn.url = _root.core.server;
+    _root.conn.login = function(strUsername, strPassword)
+    ACTIONSCRIPT,
+];
+
+$file = "game15_9_00.swf";
+$outfile = "{$file}-patched.swf";
+
+$scriptsDir = "{$file}-scripts";
+$replacedScriptsDir = "{$file}-patched-scripts";
+if(!\is_dir("{$file}-scripts")) {
+    \mkdir("{$file}-scripts");
+
+    // first export all the scripts to a folder
+    $cmd = "java -jar \"C:\\Program Files (x86)\\FFDec\\ffdec.jar\" -export script \"{$file}-scripts\" {$file}\n";
+
+    echo "Run the following command:\n";
+    echo "{$cmd}\n";
+    die;
+}
+
+if(!\is_dir($replacedScriptsDir)) {
+    \mkdir($replacedScriptsDir);
+    // then replace all the occurrences of the keys with their values
+    $scripts = globR("{$file}-scripts");
+    foreach ($scripts as $script) {
+        $content = \file_get_contents($script);
+        $newContent = \str_replace(\array_keys($replaces), \array_values($replaces), $content);
+        if($content === $newContent) {
+            continue;
+        }
+
+        $newFileName = \str_replace($scriptsDir, $replacedScriptsDir, $script);
+        if(!\is_dir(\dirname($newFileName))) {
+            \mkdir(\dirname($newFileName), 0777, true);
+        }
+        \file_put_contents($newFileName, $newContent);
+    }
+}
+
+// finally, import the scripts back to the swf file
+$cmd = "java -jar \"C:\\Program Files (x86)\\FFDec\\ffdec.jar\" -importScript {$file} {$outfile} \"{$file}-patched-scripts\"\n";
+echo $cmd;
+die;
+
+function globR($dir) {
+    $files = [];
+    foreach (\scandir($dir) as $file) {
+        if ($file == '.' || $file == '..') continue;
+        $path = $dir . '/' . $file;
+        if (\is_dir($path)) {
+            $files = \array_merge($files, globR($path));
+        } else {
+            $files[] = $path;
+        }
+    }
+    return $files;
+}
