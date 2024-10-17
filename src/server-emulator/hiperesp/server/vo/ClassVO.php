a0a21566ee98c224eebaa38f1be0708a7be82eba
hiperesp <gabriel@gabstep.com.br>
2024-10-17 02:15:40 -0300

    fixed class equippable

--- src/server-emulator/hiperesp/server/vo/ClassVO.php
+++ src/server-emulator/hiperesp/server/vo/ClassVO.php
@@ -12,6 +12,6 @@ class ClassVO extends ValueObject {
     public readonly int $weaponId;
     public readonly int $savable;
 
-    public readonly int $equippable;
+    public readonly string $equippable;
 
 }
