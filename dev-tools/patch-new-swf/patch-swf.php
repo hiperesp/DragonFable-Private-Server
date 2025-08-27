26b12e4eeb427740298a43c7732d42e3350b63b1
hiperesp <gabriel@gabstep.com.br>
2025-08-27 16:25:07 -0300

    new game version 15.9.47

--- dev-tools/patch-new-swf/patch-swf.php
+++ dev-tools/patch-new-swf/patch-swf.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 define('OS', 'windows'); // windows, mac, linux
-define('SWF_FILE', 'game15_9_46');
+define('SWF_FILE', 'game15_9_47');
 
 $replaces = [
     # REPLACE SOME FRONTEND URLS TO DYNAMIC URLS
